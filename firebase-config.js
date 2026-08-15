cd42739604e87e3590812850c566975f5f96d865
specialowner <specialowner2026@gmail.com>
2026-08-16 02:40:36 +0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -6,16 +6,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
-
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "special-owner.firebaseapp.com",
-  projectId: "special-owner",
-  storageBucket: "special-owner.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCr1fNmevHZxVdagdJDAHBfqsvW8PQeWkY",
+  authDomain: "specialowner-f3dce.firebaseapp.com",
+  projectId: "specialowner-f3dce",
+  storageBucket: "specialowner-f3dce.firebasestorage.app",
+  messagingSenderId: "564954741060",
+  appId: "1:564954741060:web:e3fc609004b1f3c5f3bf11",
+  measurementId: "G-P89M3CDTPM"
 };
-
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
